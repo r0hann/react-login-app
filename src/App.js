0d844a4dd9a4963be7ced1b69a507f6613157606
rohan <rajthala.rohan@gmail.com>
2019-08-02 20:26:43 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import { Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+// import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1)
+  },
+  dense: {
+    marginTop: theme.spacing(2)
+  },
+  menu: {
+    width: 200
+  }
+}));
+
+const App = () => {
+  const [data, setData] = useState({ email: '', password: '' });
+  const classes = useStyles();
+
+  const handleChange = event => {
+    const { target } = event;
+    setData({ ...data, [target.name]: target.value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ToastContainer />
+      <main className='container'>
+        <Card className={classes.card}>
+          <TextField
+            label='Email'
+            name='email'
+            className={classes.textField}
+            value={data.username}
+            onChange={handleChange}
+            margin='normal'
+            variant='outlined'
+          />
+        </Card>
+      </main>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+// import configureStore from './redux/configStore';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const stores = configureStore();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
